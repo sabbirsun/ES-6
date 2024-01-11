bb8d598467eaef013cabac4444ef6498b34a014a
sabbirsun <sabbirsun1920@gmail.com>
2024-01-11 20:21:05 +0600

    es6

---
+++ index.js
@@ -0,0 +1,12 @@
+// let age = 23;
+// age = 25;
+// console.log(age);
+
+// for(var i = 0; i < 5; i++){
+//     console.log("Inside Loop" + i);
+// }
+// console.log("outside loop" + i);
+
+const obj = {name:"Sabbir"};
+obj.name = "Sun";
+console.log(obj.name);
