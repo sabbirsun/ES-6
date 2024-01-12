22d6ad1dbbab3f7ea3ecac912408b4d74fe6fa2b
sabbirsun <sabbirsun1920@gmail.com>
2024-01-12 19:34:43 +0600

    Spread Operator Modern

--- index.js
+++ index.js
@@ -20,7 +20,14 @@ say("Hello Sun")*/
 
 // Rest Parameters
 
-function rest(...Name){//rest(a,b, ...sun) many value
+/*function rest(...Name){//rest(a,b, ...sun) many value
     console.log(Name);
 }
-rest("Sabbir","Sun");
+rest("Sabbir","Sun");*/
+
+// Spread Operator Modern
+
+/*const arr1 = [1,2,3];
+const arr2 = [...arr1,4,5,6]; // array 1 er gulo array 2 te show korar jnno 
+console.log(arr2);*/
+
